a623ad206ac32b439fdf32821faab85dd6066680
maria <mpramos78915400@gmail.com>
2025-05-09 22:31:12 -0400

    page hme

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import './App.css'
 import Welcome from './pages/Welcome'
 
 function App() {
--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -1,43 +1,109 @@
 import React from 'react'
-import { Box } from "@mui/material";
-import fondImage from '../assets/Welcome-screen.svg'
+import { Box, Button, Typography } from "@mui/material";
+import fondImage from '../assets/welcomefond.svg'
 import backgroundImage from '../assets/imgwelcome.svg'
-import WatchIcon  from '../assets/icons/folder.svg'
+import WatchIcon from '../assets/icons/folder.svg'
 
 const Welcome = () => {
   return (
     <Box
       sx={{
-        position:'relative',
-        width:'100vw',
-        height:'100vh',
-        overflow:'hidden',
-      }}>
-
-        <Box
-          sx={{
-            position:'absolute',
-            width:'100%',
-            height:'100%',
-            // backgroundImage:`url(${fondImage})`,
-            backgroundColor:'blueviolet',
-            zIndex:1
-          }}
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+        overflow: 'hidden',
+      }}
+    >
+      <Box
+        component="img"
+        src={fondImage}
+        sx={{
+          position: 'absolute',
+          width: '100%',
+          height: '100%',
+          zIndex: 1,
+          top: 0,
+          left: 0,
+          objectFit: 'cover',
+        }}
+      >
+      </Box>
+      <Box
+        component="img"
+        src={backgroundImage}
+        alt="background"
+        sx={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100vw',
+          height: '120vh',
+          objectFit: 'cover',
+          zIndex: 2
+        }}>
+      </Box>
+          <Box
+            sx={{
+              position:'relative',
+              zIndex: 3,
+              display: 'flex',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              alignItems: 'center',
+              height: '100vh',
+              color: 'white',
+              textAlign: 'center',
+            }}
           >
-        </Box>
-        <Box
-          sx={{
-            position:'absolute',
-            width:'100%',
-            height:'100%',
-            backgroundImage:`url(${backgroundImage})`,
-            backgroundSize:'contain',
-            backgroundPosition:'center',
-            zIndex:2,
-            backgroundRepeat:'no-repeat'
-          }}>
-        </Box>
-      </Box>   
+            <Box
+              sx={{
+                display: 'flex',
+                gap: '10px',
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginBottom: '20px'}}
+                >
+            <img 
+              src={WatchIcon}
+              alt="watch icon" 
+              style={{
+                width:'50px',
+                height:'50px',
+              }}/>
+            <Typography 
+              variant='h4'
+              sx={{ 
+                fontWeight: 'bold',
+                 }}>
+                  WATCH
+              </Typography>
+            </Box>
+            <Typography 
+              variant='body1'
+              sx={{ 
+                marginBottom: '20px' }}>
+                  Enjoy the newest movies
+                </Typography>
+          <Button
+            variant="contained"
+            color="primary"
+            sx={{
+              backgroundColor: '#6100C2',
+              padding: '10px  60px',
+              marginBottom: '20px',
+              '&:hover': {
+                backgroundColor: '#3700b3',
+              },
+            }}
+          > Log in
+          </Button>
+          <Typography 
+            variant='body2'>
+                No account? {' '}
+                <a href='#' style={{ color: 'white' }}>Sign up</a>
+              </Typography>
+          </Box>
+    </Box>
   )
 }
 
